e6d6f2577753d5c5a9ea4ac9f9c9da8f6bda7f77
Jessica Swift <jessica.nicole.swift@gmail.com>
2018-11-26 15:09:48 -0600

    adding some trivia questions

---
+++ trivia.js
@@ -0,0 +1,173 @@
+var triviaQuestions = [
+  {
+    question: "What color is the couch in Central Perk?",
+    possibleAnswers: ["Purple", "Orange", "Pink"],
+    answer: "Orange"
+  },
+  {
+    question: "What band sings the opening credits song?",
+    possibleAnswers: ["The Rembrandts", "Soundgarden", "The Smashing Pumpkins"],
+    answer: "The Rembrandts"
+  },
+  {
+    question: "Who said, 'Oh I'm sorry, did my back hurt your knife?' ",
+    possibleAnswers: ["Rachel", "Monica", "Phoebe"],
+    answer: "Rachel"
+  },
+  {
+    question: "What is Phoebe's twin sister's name?",
+    possibleAnswers: ["Ursula"],
+    answer: "Ursula"
+  },
+  {
+    question:
+      "Which Friends cast memeber has never hosted Saturday Night Live?",
+    possibleAnswers: ["Jennifer Aniston", "David Schwimmer", "Matt LeBlanc"],
+    answer: "Matt LeBlanc"
+  },
+  {
+    question: "What is the 'Joey speical'?",
+    possibleAnswers: ["Meat Lovers", "2 Pizzas", "2 Pepperoni Pizzas"],
+    answer: "2 Pizzas"
+  },
+  {
+    question: "Who does Phoebe think her grandfather is?",
+    possibleAnswers: ["Albert Einstein", ""],
+    answer: "Albert Einstein"
+  },
+  {
+    question: "What did Ross say that Rachel's 'traditional English trifle' taste like?",
+    possibleAnswers: ["Feet"],
+    answer: "Feet"
+  },
+  {
+    question: "How many categories for towels does Monica have?",
+    possibleAnswers: ["7", "3", "11"],
+    answer: "11"
+  },
+  {
+    question: "What is Chandler's dad's burlesque show called?",
+    possibleAnswers: ["Viva Las Loveless", "Viva Las Gaygas", "Viva Las MenLess"],
+    answer: "Viva Las Gaygas"
+  },
+  {
+    question: "Which high school did Ross, Monica and Rachel attend?",
+    possibleAnswers: ["Lincoln High School"],
+    answer: "Lincoln High School"
+  },
+  {
+    question: "What is Monica's biggest pet peeve?",
+    possibleAnswers: ["Someone else cleaning her home", "Animals dressed as humans", "Glass rings on coffee tables"],
+    answer: "Animals dressed as humans"
+  },
+  {
+    question: "What is the name on Chandler and Joey's TV Guide?",
+    possibleAnswers: ["Miss Chanandler Bong"],
+    answer: "Miss Chanandler Bong"
+  },
+  {
+    question: "What fruit is Ross allergic to?",
+    possibleAnswers: ["apple", "banana", "kiwi"],
+    answer: "kiwi"
+  },
+  {
+    question: "What kind of animal is Huggsy?",
+    possibleAnswers: ["Hippo", "Penguin", 'Bear'],
+    answer: "Penguin"
+  },
+  {
+    question: "Which guest star did not win an Emmy for their performances?",
+    possibleAnswers: ["Christina Applegate", "Bruce Willis", "Brad Pitt"],
+    answer: "Brad Pitt"
+  },
+  {
+    question: "What sport and position did Monica play in high school?",
+    possibleAnswers: ["Field hockey goalie"],
+    answer: "Field hockey goalie"
+  },
+  {
+    question: "What is Chandler's job?",
+    possibleAnswers: ["IT procurement manager"],
+    answer: "IT procurement manager"
+  },
+  {
+    question: "Who terrifies Chandler?",
+    possibleAnswers: ["Michael Flatley, Lord of the Dance"],
+    answer: "Michael Flatley, Lord of the Dance"
+  },
+  {
+    question: "Who pees on Monica after she is stung by a jelly fish?",
+    possibleAnswers: ["Ross", "Joey", "Chandler"],
+    answer: "Chandler"
+  },
+  {
+    question: "Who got a pencil stuck in his/her ear?",
+    possibleAnswers: ["Chandler", "Monica", "Rachel"],
+    answer: "Monica"
+  },
+  {
+    question: "What was the musical Joey starred in?",
+    possibleAnswers: ["The Musical", "Freud", "Sigmund Freud"],
+    answer: "Freud"
+  },
+  {
+    question: "What movie did Ross claim to have come up with the idea for?",
+    possibleAnswers: ["The land before time", "We're back", "Jurassic Park"],
+    answer: "Jurassic Park"
+  },
+  {
+    question: "",
+    possibleAnswers: [],
+    answer: ""
+  },
+  {
+    question: "",
+    possibleAnswers: [],
+    answer: ""
+  },
+  {
+    question: "",
+    possibleAnswers: [],
+    answer: ""
+  },
+  {
+    question: "",
+    possibleAnswers: [],
+    answer: ""
+  },
+  {
+    question: "",
+    possibleAnswers: [],
+    answer: ""
+  },
+  {
+    question: "",
+    possibleAnswers: [],
+    answer: ""
+  },
+  {
+    question: "",
+    possibleAnswers: [],
+    answer: ""
+  },
+  {
+    question: "",
+    possibleAnswers: [],
+    answer: ""
+  },
+  {
+    question: "",
+    possibleAnswers: [],
+    answer: ""
+  },
+  {
+    question: "",
+    possibleAnswers: [],
+    answer: ""
+  },
+  {
+    question: "",
+    possibleAnswers: [],
+    answer: ""
+  }
+];
